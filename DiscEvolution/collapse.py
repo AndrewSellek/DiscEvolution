ee28ec5d04fa6c1d5e3af8b33daca6be221a3aff
AndrewSellek <sellek@strw.leidenuniv.nl>
2024-07-02 09:17:51 +0200

    Try to regulate the chemistry to avoid total masses more than unity

--- DiscEvolution/collapse.py
+++ DiscEvolution/collapse.py
@@ -44,6 +44,8 @@ class CollapseBase(object):
                 if disc.chem:
                     for k in self.chem.keys():
                         disc.chem.ice[k] += (self.chem[k]-disc.chem.ice[k])*dSS/(1+dSS)
+                    disc.update_ices(disc.chem.ice)
+                    disc.update(dt)
                 elif hasattr(disc, "integ_dust_frac"):
                     disc._eps[0] += (self._dust_frac*(1-disc._fm)-disc._eps[0])*dSS/(1+dSS)
                     disc._eps[1] += (self._dust_frac*disc._fm-disc._eps[1])*dSS/(1+dSS)
