Corrected UV optical depth calculation, add floor to G0 used for chemistry to external field value (default 0)

DiscEvolution/chemistry/Extended_chem.py
@@ -256,8 +256,8 @@ class ChemExtended(object):
         sputterYield = 8e-4 # Alata+14/15, Anderson+17, Bosman+21
         vol_per_C = 12*m_H/2.24
         k_grain = 0.75*vol_per_C*sputterYield*F_UV/grainSize
-        tau_UV = 2.6e-21*d2g[0]*NH2 # Extinction in upper layers - assume small dust only
-        f_small = d2g[0]/d2g.sum(0) # Fraction in samll dust
+        tau_UV = 2.6e-21*(d2g[0]/0.01)*2*NH2 # Extinction in upper layers - assume small dust only
+        f_small = d2g[0]/d2g.sum(0) # Fraction in small dust
         f_exposed = (1-np.exp(-tau_UV))/tau_UV
         return k_grain*f_small*f_exposed # rate per grain * fraction of grains that are vertically mixed * fraction vertically above tau=1
         

DiscEvolution/driver.py
@@ -161,7 +161,8 @@ class DiscEvolutionDriver(object):
             f_small[(eps==0.0)] = 0.0
             grain_size = disc.grain_size[-1]
             T = disc.T
-            F_UV = disc.star.LFUV/(4*np.pi*(disc.R*AU)**2) * disc.angleFlare_FUV * 1e8/1.63e-3
+            LFUV_G0 = disc.star.LFUV/(4*np.pi*(disc.R*AU)**2) * disc.angleFlare_FUV/1.63e-3 + disc.FUV
+            F_UV = LFUV_G0 * 1e8
 
             self._chemistry.update(dt, T, rho, eps, f_small, disc.R, disc.Sigma_G, disc.chem, F_UV, grain_size=grain_size)
 